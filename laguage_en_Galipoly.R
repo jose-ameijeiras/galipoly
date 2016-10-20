30823ae6ba461a6db69b309cd2e75a7dcb067fb8
Jose Ameijeiras-Alonso <jose.ameijeiras@usc.es>
2016-10-20 13:27:53 +0200

    Add files via upload

---
+++ laguage_en_Galipoly.R
@@ -0,0 +1,101 @@
+#Special streets
+namechance="Chance"
+namecomchest="Community chest"
+njails=c("Jail. Just visiting","Go to jail")
+namego="Go"
+#To appear on go (Collect ...)
+sentencein1="Collect"
+
+
+###########################################################################
+
+#Messages
+
+#Number of players
+msgnumberpl="Select the number of players"
+
+#Introduce their names
+msgnamepl="Introduce their names"
+
+#Select the coin
+msgcoin="Select the coin:"
+
+
+#Mesagge to throw dices
+msgdice1="is your turn. Press OK to start."
+
+#Messages to end the game
+msgend1="End of your turn"
+msgend2="Next player"
+msgend3="End of the game."
+msgend4="Leave the game (just the actual player)."
+msgend5="decided to throw away the board. End of the game."
+msgend6="leave the game"
+msgend7="is broke. Sell or mortgage streets."
+msgend8="The game is over."
+msgend9="won the game."
+
+#Message to buy
+msgtobuy1="Do you want to buy "
+msgtobuy2="?"
+msgtobuy3="No"
+msgtobuy4="Yes"
+
+#Messages in jail
+msgjail1="Get out of jail."
+msgjail2="Keep in jail."
+msgjail3="Pay a fine."
+msgjail4="Use a Chance Get Out of Jail Free card."
+msgjail5="Use a Community Chest Get Out of Jail Free card."
+
+#Messages if doubles
+msgdoubles1="You rolls doubles, then roll again."
+msgdoubles2="You rolls doubles three times. Go to jail."
+
+#Messages to build
+msgbuild1="Not to build."
+msgbuild2="You can build in the following streets:"
+msgbuild3="has"
+msgbuild4="house."
+msgbuild5="houses."
+msgbuild6="hotel."
+msgbuild7="Select the number of houses that you want in that street:"
+
+#Messages to sell
+msgsell1="Select a street to sell, mortgage or unmortgage."
+msgsell2="Not make any action"
+msgsell4="Collect"
+msgsell5="if you sell."
+msgsell6="if you mortgage."
+msgsell7="Pay"
+msgsell8="if you want to unmortgage."
+msgsell9="for each house that you sell."
+msgsell10="if you sell a hotel."
+msgsell12="(mortgaged)"
+msgsell13="Sell"
+msgsell14="This action is not doable."
+msgsell15="You have"
+msgsell16=msgbuild4
+msgsell17=msgbuild5
+msgsell18=msgbuild6
+msgsell19="Sell the street."
+msgsell20="Mortgage the street."
+msgsell21="Unmortgage the street."
+
+#########################################################################
+
+#In the boxes inside the board
+
+#For writting in the cards
+chancewords=c("Back three positions","Go to jail","Advance to the street","This card has no efect","Get out of jail free")
+comchwords=c(,"Go to jail","Advance to the street","Get out of jail free")
+cchmonwords=c("Collect","Pay","to each player","per house and","per hotel")
+
+#Money
+namemoney="Money"
+
+#Generic names
+namegen="Player"
+
+
+
