4bbf956d515753c47321d249b2309bd9c36cee80
Jose Ameijeiras-Alonso <jose.ameijeiras@usc.es>
2016-10-20 13:28:47 +0200

    Update game_Galipoly.R

--- game_Galipoly.R
+++ game_Galipoly.R
@@ -34,8 +34,7 @@ source(foldergrapint)
 
 
 #Script with the language
-languagesavail=c("Castellano")
-#languagesavail=c("Castellano","Galego","English")
+languagesavail=c("Castellano","Galego","English")
 languageChoice=GI1()
 if(languageChoice==1){
 folderlan=paste(folder,"laguage_es_Galipoly.R",sep="")}
