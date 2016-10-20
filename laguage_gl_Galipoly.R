d2cbfa6f75817d1efa86b452e05aad2e7d6c237e
Jose Ameijeiras-Alonso <jose.ameijeiras@usc.es>
2016-10-20 12:54:17 +0200

    Add files via upload

---
+++ laguage_gl_Galipoly.R
@@ -0,0 +1,101 @@
+#Special streets
+namechance="Sorte"
+namecomchest="Caixa de comunidade"
+njails=c("Cárcere. Soamente visitas","Vai ó cárcere")
+namego="Saída"
+#To appear on go (Collect ...)
+sentencein1="Cobre"
+
+
+###########################################################################
+
+#Messages
+
+#Number of players
+msgnumberpl="Seleccione o número de xogadores"
+
+#Introduce their names
+msgnamepl="Introduza o nome dos xogadores"
+
+#Select the coin
+msgcoin="Seleccione o tipo de moeda:"
+
+
+#Mesagge to throw dices
+msgdice1="é a súa quenda. Prense OK para comezalo."
+
+#Messages to end the game
+msgend1="Fin da súa quenda"
+msgend2="Pasala quenda ó seguinte xogador"
+msgend3="Terminala partida."
+msgend4="Saír do xogo (unicamente o xogador actual)."
+msgend5="lanzou o taboleiro polos aires. Fin do xogo."
+msgend6="abandonou o xogo"
+msgend7="está sen diñeiro. Venda ou hipoteque propiedadades."
+msgend8="Terminou o xogo."
+msgend9="gañou a partida."
+
+#Message to buy
+msgtobuy1="¿Quere comprar "
+msgtobuy2="?"
+msgtobuy3="Non"
+msgtobuy4="Si"
+
+#Messages in jail
+msgjail1="Saír da prisión."
+msgjail2="Permanecer na prisión"
+msgjail3="Pagar para saír do cárcere."
+msgjail4="Empregar a carta de sorte para saír do cárcere"
+msgjail5="Empregar a carta de caixa de comunidade para saír do cárcere"
+
+#Messages if doubles
+msgdoubles1="Sacaches dobres. Obtés unha nova quenda."
+msgdoubles2="Sacaches tres veces dobres. Ve a prisión."
+
+#Messages to build
+msgbuild1="Non edificar."
+msgbuild2="Pode edificar nos seguintes solares:"
+msgbuild3="ten"
+msgbuild4="casa."
+msgbuild5="casas."
+msgbuild6="hotel."
+msgbuild7="Seleccione o número de casas que quere ter en esa propiedade:"
+
+#Messages to sell
+msgsell1="Seleccione o terreo para vender, hipotecar ou deshipotecar."
+msgsell2="Non realizar ningunha acción"
+msgsell4="Cobre"
+msgsell5="por vender."
+msgsell6="por hipotecar."
+msgsell7="Pague"
+msgsell8="para levantala hipoteca."
+msgsell9="por cada casa que venda."
+msgsell10="se vende un hotel."
+msgsell12="(hipotecada)"
+msgsell13="Vender"
+msgsell14="Non se pode realizar esa acción."
+msgsell15="Ten"
+msgsell16=msgbuild4
+msgsell17=msgbuild5
+msgsell18=msgbuild6
+msgsell19="Vender a propiedade."
+msgsell20="Hipotecar a propiedade."
+msgsell21="Levantar a hipoteca."
+
+#########################################################################
+
+#In the boxes inside the board
+
+#For writting in the cards
+chancewords=c("Retrocede tres casas","Vai ó cárcere","Desprazaste á casa","Esta carta no ten efecto","Con esta carta saes de prisión")
+comchwords=c("Vai ó cárcere","Desprazaste á casa","Con esta carta saes de prisión")
+cchmonwords=c("Cobre","Pague","a cada xogador","por casa e","por hotel")
+
+#Money
+namemoney="Diñeiro"
+
+#Generic names
+namegen="Xogador"
+
+
+
